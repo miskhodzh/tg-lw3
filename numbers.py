4b89a4f3c653d42010a9a08357079d1e734b3557
miskhodzh <miskhodzhev.osman@yandex.ru>
2022-10-17 17:41:40 +0300

    added a program numbers.py

---
+++ numbers.py
@@ -0,0 +1,11 @@
+n1 = input('Введите число 1 >>> ')
+n2 = input('Введите число 2 >>> ')
+n3 = input('Введите число 3 >>> ')
+n4 = input('Введите число 4 >>> ')
+
+sum1 = int(n1) + int(n2)
+sum2 = int(n3) + int(n4)
+
+ratio = sum1/sum2
+
+print("%.2f" % ratio)
